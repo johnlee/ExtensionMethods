7cd51ba7f8b5ad9d5a87c63dea44b0040ab9a0c7
Lee, John <lee1048@llnl.gov>
2019-12-05 14:25:33 -0800

    Initial Commit

---
+++ Extender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace System
+{
+    public static class Extensions
+    {
+        public static bool IsLengthEven(this String str)
+        {
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace DnetExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example of Extension Metods");
+
+            var e1 = new List<string> { "a", "b", "c" };
+
+            var r1 = e1.Count(); // .net built in ext method from System.Linq
+
+            Console.WriteLine($"R1 = {r1}");
+
+            var e2 = "Hello World";
+
+            var r2 = e2.IsLengthEven();
+
+            Console.WriteLine($"R2 = {r2}");
+        }
+    }
+}
